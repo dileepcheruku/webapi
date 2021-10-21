ee76bc7b7b0e34ed8a5daf1d9fc6d409ae1bbf4a
dileepcheruku <92827751+dileepcheruku@users.noreply.github.com>
2021-10-21 00:21:41 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "aws"  {
   region  =  "us-east-2"
-  access_key = "AKIAY47IF5YE2DNAJ2MF"
-  secret_key = "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"
+  access_key = ${{ secrets.AWS_ACCESS_KEY }}
+  secret_key = ${{ secrets.AWS_SECRET_KEY }}
 }
