87ee418b1936aab8a4f912c7700313ea435d95c3
dileepcheruku <92827751+dileepcheruku@users.noreply.github.com>
2021-10-21 00:22:28 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "aws"  {
   region  =  "us-east-2"
-  access_key = ${{ secrets.AWS_ACCESS_KEY }}
-  secret_key = ${{ secrets.AWS_SECRET_KEY }}
+  access_key = ${{ secrets.AWS_ACCESS_KEY_ID }}
+  secret_key = ${{ secrets.AWS_SECRET_ACCESS_KEY }}
 }
