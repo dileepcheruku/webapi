8337865ff3a52174342bb14afd95355a5b2a4829
dileepcheruku <92827751+dileepcheruku@users.noreply.github.com>
2021-10-21 00:14:28 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "aws"  {
   region  =  "us-east-2"
-  access_key = "AKIAY47IF5YEXVCQMTZA"
-  secret_key = "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"
+  access_key = "AKIAY47IF5YE2DNAJ2MF"
+  secret_key = "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"
 }
