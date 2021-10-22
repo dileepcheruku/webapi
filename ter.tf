73685e6ffbbebc79fe21392f82225f99abd74883
dileepcheruku <92827751+dileepcheruku@users.noreply.github.com>
2021-10-22 17:41:45 -0400

    Update ter.tf

--- ter.tf
+++ ter.tf
@@ -1,18 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.27"
-    }
-  }
-
-  required_version = ">= 0.14.9"
-}
-
-provider "aws" {
-  profile = "default"
-  region  = "us-east-2"
-}
 
 resource "aws_instance" "app_server" {
   ami             = "ami-0277b52859bac6f4b"
