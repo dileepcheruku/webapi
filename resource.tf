1c6a50953e0ade37a709e948ab2f81ba04acfd3a
dileepcheruku <92827751+dileepcheruku@users.noreply.github.com>
2021-10-22 19:47:05 -0400

    Update resource.tf

--- resource.tf
+++ resource.tf
@@ -56,7 +56,7 @@ resource "aws_network_interface" "foo" {
       from_port        = 443
       to_port          = 443
       protocol         = "tcp"
-      cidr_blocks      = [0.0.0.0/0]
+      cidr_blocks      = ["0.0.0.0/0"]
       ipv6_cidr_blocks = []
     }
   ]
