35d106751cc4812e2af45aa5d8be80f3b6a6049f
dileepcheruku <92827751+dileepcheruku@users.noreply.github.com>
2021-10-22 21:48:55 -0400

    Update resource.tf

--- resource.tf
+++ resource.tf
@@ -55,7 +55,7 @@ resource "aws_network_interface" "foo" {
 resource "aws_instance" "foo" {
   ami             = "ami-074cce78125f09d61" 
   instance_type   = "t2.micro"
-  key_name        = "vicky"
+  key_name        = "foo"
   user_data	= file("file.sh")
   
  
